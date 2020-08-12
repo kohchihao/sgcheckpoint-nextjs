88f2241f490c79afe9df055f3f45d3a9cdedd19e
Marcus Koh <kohchihao@gmail.com>
2020-08-12 19:03:56 +0800

    Update site url

--- utils/siteUrl.js
+++ utils/siteUrl.js
@@ -3,7 +3,7 @@ let BASE_URL = '';
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = 'http://localhost:3000';
 } else {
-  BASE_URL = 'https://sgcheckpoint.com';
+  BASE_URL = 'https://sgcheckpoint-nextjs.vercel.app/';
 }
 
 export { BASE_URL };
